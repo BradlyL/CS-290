7101230d62a0d74ee473e88fc892dd298a566a69
Bradly landucci <bradly.landucci.5149@mail.linnbenton.edu>
2021-02-25 15:16:57 -0800

    One form done

--- Website Weeks 7-8/Index_server.py
+++ Website Weeks 7-8/Index_server.py
@@ -1,9 +1,14 @@
 import http.server
 import socketserver
 
-PORT = 8000
+#Not quite sure if I can make this page much different than your own code. seems straight forward.
+
+Server_PORT = 8000
 handler = http.server.CGIHTTPRequestHandler
 
-with http.server.HTTPServer(("", PORT), handler) as httpd:
-    print(f"Server starting on port {PORT}")
-    httpd.serve_forever()
+
+#Tried to clean up as far as I could go, or just doing it in a different way. could go further to just remove variables since I'm not planning on changing anything.
+#I suppose that would make it just less appealing to the eyes though.
+
+if __name__ == "__main__":
+    http.server.HTTPServer(("", Server_PORT), handler).serve_forever()
--- Website Weeks 7-8/cgi-bin/cgi_script.py
+++ Website Weeks 7-8/cgi-bin/cgi_script.py
@@ -1,29 +1,26 @@
-#!/usr/bin/python 
-  
-# Import CGI and CGIT module 
-import cgi, cgitb
+import cgi, cgitb #importing cgi module, upon some reading cgitb does some detailed reports within your web browser could be useful
+#https://docs.python.org/3/library/cgi.html
 
-# to create instance of FieldStorage  
-# class which we can use to work  
-# with the submitted form data 
-form = cgi.FieldStorage()       
-password_input = form.getvalue("password_input", "Empty Form")     
+form = cgi.FieldStorage() #Stores value
+password_input = form.getvalue("password_input", "Empty Form") #Grabs value from form
   
 print ("Content-type:text/html")
 print ("<html>") 
 print ("<head>") 
 print ("<title>Bradly Landucci</title>") 
 print ("</head>") 
-
-
 print ("<body>") 
+
+#Recursively calls file and changes text on screen.
 print ("""
 <form method="post" action = "cgi_script.py">
 <p>
        Replaces previous message below: 
-       <input type = "text" name = "password_input">
+       <input type = "text" name = "password_input"> 
 </p>
 </form> """)
+
+#Prints h1 input, Kind of weird that it is saving my previous inputs even after I restart the web server. Not sure how to reset this.
 print("<h1>%s</h1>" % (password_input))
 print ("</form>")
 print ("</body>")
