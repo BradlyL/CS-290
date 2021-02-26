d480babe57f9c833dee82875190ea0ff7f81f227
Bradly landucci <bradly.landucci.5149@mail.linnbenton.edu>
2021-02-25 17:31:06 -0800

    Bleh

--- Website Weeks 7-8/Index_server.py
+++ Website Weeks 7-8/Index_server.py
@@ -6,7 +6,6 @@ import socketserver
 Server_PORT = 8000
 handler = http.server.CGIHTTPRequestHandler
 
-
 #Tried to clean up as far as I could go, or just doing it in a different way. could go further to just remove variables since I'm not planning on changing anything.
 #I suppose that would make it just less appealing to the eyes though.
 
--- Website Weeks 7-8/cgi-bin/cgi_script.py
+++ Website Weeks 7-8/cgi-bin/cgi_script.py
@@ -3,15 +3,37 @@ import cgi, cgitb #importing cgi module, upon some reading cgitb does some detai
 
 form = cgi.FieldStorage() #Stores value
 password_input = form.getvalue("password_input", "Empty Form") #Grabs value from form
-  
 print ("Content-type:text/html")
 print ("<html>") 
 print ("<head>") 
 print ("<title>Bradly Landucci</title>") 
+
+print ("""
+<script>
+function pass_input() {
+    var submit_value;  // Kept trying to run this variable outside of function and I realized why that didn't make sense..
+    var text;
+    var password_1 = "password";
+
+    submit_value = document.getElementById("code").value;
+   
+    if (submit_value == password_1) {
+        text = "Correct."
+        window.location.href = './Subpages/Subpage_1.html';
+    } else {
+        text = "Incorrect.";
+    }
+
+    document.getElementById("password_reveal").innerHTML = text;
+    console.log("Password_input function is working.");
+}
+</script>""")
+
 print ("</head>") 
+
 print ("<body>") 
 
-#Recursively calls file and changes text on screen.
+#Form #1 Recursively calls file and changes text on screen.
 print ("""
 <form method="post" action = "cgi_script.py">
 <p>
@@ -20,8 +42,17 @@ print ("""
 </p>
 </form> """)
 
-#Prints h1 input, Kind of weird that it is saving my previous inputs even after I restart the web server. Not sure how to reset this.
 print("<h1>%s</h1>" % (password_input))
-print ("</form>")
+
+#Form #2 Password enterer
+print ("""
+<p>
+       Replaces previous message below: 
+       <input type ="code"> 
+       <button type="button" onclick="pass_input()">Submit</button>
+</p>
+""")
+
+#Prints h1 input, Kind of weird that it is saving my previous inputs even after I restart the web server. Not sure how to reset this.
 print ("</body>")
 print ("</html>")
